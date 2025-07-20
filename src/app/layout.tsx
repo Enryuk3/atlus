4ec3d8d5366009551a757d4c8de99a42b1abd2f7
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-07-20 18:41:19 -0500

    update bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased`}>
+      <body className={`antialiased bg-black text-gray-300`}>
         <Header />
         {children}
       </body>
