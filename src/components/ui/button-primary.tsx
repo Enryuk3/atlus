8336ab168a3f685ee02822affc8020aa3977c62e
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-07-20 04:57:12 -0500

    add atom components

---
+++ src/components/ui/button-primary.tsx
@@ -0,0 +1,19 @@
+
+import { cn } from "@/lib/utils";
+import { FC } from "react";
+
+interface ButtonPrimaryProps {
+  type: 'button' | 'submit';
+  text: string;
+  className?: string;
+}
+
+const ButtonPrimary: FC<ButtonPrimaryProps> = ({ type, text, className }) => {
+  return (
+    <button type={type} className={cn('px-4 py-2 bg-primary text-white rounded-lg border-2 border-transparent hover:bg-white hover:border-primary hover:text-primary transition-colors duration-300', className)}>
+      {text}
+    </button >
+  );
+}
+
+export default ButtonPrimary;
